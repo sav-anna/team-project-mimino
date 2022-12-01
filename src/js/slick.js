57fc43aa400cc62e6ba8d7e98e5db6c1865da1e3
Andrii <andrii.krymchak@gmail.com>
2022-12-02 00:54:37 +0200

    add img and fix some bugs

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,5 +1,5 @@
 $('.one-time').slick({
-  // dots: true,
+  dots: true,
   prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
   nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
   infinite: true,
