f2ceb316fb97ff1013f2d7a40c193cea4031a914
Andrii <andrii.krymchak@gmail.com>
2022-12-02 15:36:48 +0200

    correct img width

--- src/js/slick-details-hotel.js
+++ src/js/slick-details-hotel.js
@@ -6,20 +6,16 @@ $('.variable-slider').slick({
   centerMode: true,
   variableWidth: true,
 mobileFirst: true,
-  
-    responsive: [
-  
-    {
+      responsive: [
+      {
       breakpoint: 1280,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 1,
-        
-        
+              
       }
         },
-    
-    {
+        {
       breakpoint: 1024,
       settings: {
         slidesToShow: 1,
@@ -28,8 +24,7 @@ mobileFirst: true,
         
       }
     },
-   
-    
+      
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
