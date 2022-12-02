cf1c43410af052cee698608efc70890cd11d8f9d
Andrii <andrii.krymchak@gmail.com>
2022-12-02 14:16:07 +0200

    add slider first step

--- src/js/slick-details-hotel.js
+++ src/js/slick-details-hotel.js
@@ -1,11 +1,38 @@
-$('.variable').slick({
-    dots: true,
-    prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
-  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
+$('.variable-slider').slick({
+  dots: false,
   infinite: true,
   speed: 300,
   slidesToShow: 1,
   centerMode: true,
-    variableWidth: true,
+  variableWidth: true,
+mobileFirst: true,
   
+    responsive: [
+  
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        
+        
+      }
+        },
+    
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        
+      }
+    },
+   
+    
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
+  
