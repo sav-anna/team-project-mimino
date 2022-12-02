757abf3bcd2bd78c43169e3a1dd79f81e9709720
AnnaVS8 <anna_sibova@ukr.net>
2022-12-02 20:37:39 +0000

    Deploying to gh-pages from @ AnnaVS8/team-project-mimino@7bb79698b14ba5b250faa9cb493d764f0010ed18 🚀

---
+++ hotel.26ed7c07.js
@@ -0,0 +1,2 @@
+$(".onetime").slick({dots:!1,prevArrow:'<button type="button" class="my-slick-prev">&xlarr;</button>',nextArrow:'<button type="button" class="my-slick-next">&xrarr;</button>',infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0}),$(".variable-slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,centerMode:!0,variableWidth:!0,mobileFirst:!0,responsive:[{breakpoint:1280,settings:{slidesToShow:3,slidesToScroll:1}},{breakpoint:1024,settings:{slidesToShow:1,slidesToScroll:1,infinite:!0}}]});
+//# sourceMappingURL=hotel.26ed7c07.js.map
