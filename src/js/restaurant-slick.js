85713fbc4f1778519b82b503558c5c7cc7fc6eb7
Ruslan Garmash <garmash.rb@gmail.com>
2022-12-08 12:23:42 +0200

    add opacity

--- src/js/restaurant-slick.js
+++ src/js/restaurant-slick.js
@@ -1,21 +1,21 @@
-$('.restaurant-slick').slick({
+$('.restaurant-slick, restaurant-kitchen-slick').slick({
   dots: false,
   infinite: true,
-  speed: 300,
+  speed: 500,
   slidesToShow: 2,
-    centerMode: false,
-    variableWidth: true,
-    
-  
+  // centerMode: true,
+  variableWidth: true,
+
   mobileFirst: true,
   responsive: [
-  
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
       },
     },
   ],
