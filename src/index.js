d4331b5b410320e1b51a19b54391e265d3adc8a3
Andrii <andrii.krymchak@gmail.com>
2022-12-10 17:35:56 +0200

    maih hotel correction with slider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './js/modal';
-import './js/slick';
-import './js/slick-details-hotel';
 import './js/hotel-slick';
 import './js/restaurant-slick';
 import './js/anchor-scroll';
--- src/js/slick.js
+++
@@ -1,9 +0,0 @@
-$('.onetime').slick({
-  dots: false,
-  prevArrow: '<button type="button" class="my-slick-prev"></button>',
-  nextArrow: '<button type="button" class="my-slick-next"></button>',
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-});
